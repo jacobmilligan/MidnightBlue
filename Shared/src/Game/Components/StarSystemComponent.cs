29bdb9f6723c1c3dd4b1000d0b98772d02e3e2e4
Jacob Milligan <jacobpmilligan@gmail.com>
2016-10-05 14:31:05 +1100

    Add: Star system generation

---
+++ Shared/src/Game/Components/StarSystemComponent.cs
@@ -0,0 +1,21 @@
+﻿//
+// 	StarSystem.cs
+// 	Midnight Blue
+//
+// 	--------------------------------------------------------------
+//
+// 	Created by Jacob Milligan on 5/10/2016.
+// 	Copyright (c) Jacob Milligan All rights reserved
+//
+using System;
+using MidnightBlue.Engine.EntityComponent;
+
+namespace MidnightBlue
+{
+  public class StarSystem : IComponent
+  {
+    public StarSystem()
+    {
+    }
+  }
+}
---
+++ Shared/src/Game/Gen/GalaxyBuilder.cs
@@ -0,0 +1,19 @@
+﻿//
+// 	GalaxyBuilder.cs
+// 	Midnight Blue
+//
+// 	--------------------------------------------------------------
+//
+// 	Created by Jacob Milligan on 5/10/2016.
+// 	Copyright (c) Jacob Milligan All rights reserved
+//
+using System;
+namespace MidnightBlue.Engine
+{
+  public class GalaxyBuilder
+  {
+    public GalaxyBuilder()
+    {
+    }
+  }
+}
